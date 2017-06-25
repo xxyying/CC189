d1e0d87164aa496b1f4e8042fae8f1f45bfe9abd
Yiying Liu <yiying@10.0.0.9>
2017-06-24 23:31:10 -0700

    - chapter 1 updated

--- src/com/xxyyCC189/algo/chapter1/OneWay.java
+++ src/com/xxyyCC189/algo/chapter1/OneWay.java
@@ -1,7 +1,23 @@
 package com.xxyyCC189.algo.chapter1;
 
-public class OneWay {
+/*
+ * 1.5 One Way
+ * e.g.
+ * pale, ple --- true
+ * pales, pale --- true
+ * pale, bale --- true
+ * pale, bake --- false
+ * 
+ */
 
+public class OneWay {
+	public boolean isOneWay(String str1, String str2) {
+		if ((str1 == null && str2 == null) || (str1.length() == 0 && str2.length() == 0)) {
+			return true;
+		}
+		
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 

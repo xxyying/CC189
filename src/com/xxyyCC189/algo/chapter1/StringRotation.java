4046bb4a2087870ea7148045d11cbde602aa867d
Yiying Liu <yiying@10.0.0.9>
2017-07-05 20:34:31 -0700

    - 07/05 cc189 update

--- src/com/xxyyCC189/algo/chapter1/StringRotation.java
+++ src/com/xxyyCC189/algo/chapter1/StringRotation.java
@@ -1,10 +1,32 @@
 package com.xxyyCC189.algo.chapter1;
 
 public class StringRotation {
-
+	public boolean stringRotation(String s1, String s2) {
+		String sResult = s1 + s1;
+		
+		if (s1.length() == s2.length() && s1.length() != 0) {
+			return isSubstring(sResult, s2);
+		}
+		return false;
+	}
+	
+	private boolean isSubstring (String a, String b) {
+		boolean findSubstring = false;
+		for(int i = 0; i < a.length() - b.length() + 1; i++) {
+			if (a.substring(i, i + b.length()).equals(b)) {
+				findSubstring = true;
+			}
+		}
+		return findSubstring;
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringRotation test = new StringRotation();
+		boolean result = test.stringRotation("waterbottle", "erbottlewat");
+		System.out.println(result);
+		
+		result = test.stringRotation("waterbottle", "erbottle");
+		System.out.println(result);
 	}
 
 }
--- src/com/xxyyCC189/algo/chapter2/C2S1RemoveDup.java
+++ src/com/xxyyCC189/algo/chapter2/C2S1RemoveDup.java
@@ -1,6 +1,6 @@
 package com.xxyyCC189.algo.chapter2;
 
-public class S1RemoveDup {
+public class C2S1RemoveDup {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub

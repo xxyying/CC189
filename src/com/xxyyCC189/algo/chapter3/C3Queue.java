9f3b63ccd08bb07789c3f8cfb7cfb244f24e7cb3
Yiying Liu <yiying@10.228.245.34>
2017-07-09 18:12:18 -0700

    - cc189 chapter3 updated

---
+++ src/com/xxyyCC189/algo/chapter3/C3Queue.java
@@ -0,0 +1,53 @@
+package com.xxyyCC189.algo.chapter3;
+
+import java.util.NoSuchElementException;
+
+public class C3Queue<T> {
+	public static class QueueNode<T> {
+		private T data;
+		private QueueNode<T> next;
+		
+		public QueueNode(T data) {
+			this.data = data;
+		}
+	}
+	
+	private QueueNode<T> first;
+	private QueueNode<T> last;
+	
+	public void add(T item) {
+		QueueNode<T> t = new QueueNode<T>(item);
+		if (last != null) {
+			last.next = t;
+		}
+		last = t;
+		if (first == null) {
+			first = last;
+		}
+	}
+	
+	public T remove(T item) {
+		if (first == null) {
+			throw new NoSuchElementException();
+		}
+		T data = first.data;
+		first = first.next;
+		if (first == null) {
+			last = null;
+		}
+		return data;
+	}
+	
+	public T peek() {
+		if (first == null) {
+			throw new NoSuchElementException();
+		}
+		return first.data;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	
+}

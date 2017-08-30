1e21344081268549aa9c348396ade0a5cf7f57b8
Yiying Liu <yiying@192.168.0.20>
2017-08-30 16:41:42 -0700

    - latest version

--- src/leetcode/L077Combination.java
+++ src/leetcode/L077Combination.java
@@ -1,10 +1,42 @@
 package leetcode;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+
 public class L077Combination {
-
+	public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (k < 1) {
+            return result;
+        }
+        ArrayList<Integer> path = new ArrayList<>();
+//        HashSet<ArrayList<Integer>> set = new HashSet<>();
+        dfs(1, n, k, path, result);
+        return result;
+    }
+    
+    private void dfs(int start, int n, int k, ArrayList<Integer> path, List<List<Integer>> result) {
+        if(path.size() == k) {
+        		result.add(new ArrayList<Integer>(path));
+        		return;
+        }
+        
+        for(int i = start; i <= n; i++) {
+            path.add(i);
+            dfs(i + 1, n, k, path, result);
+            path.remove(path.size() - 1);        		
+        }
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		L077Combination test = new L077Combination();
+		int n = 4;
+		int k = 2;
+		List<List<Integer>> result = test.combine(n, k);
+		for(List<Integer> l : result) {
+			System.out.println(l);
+		}
 	}
 
 }
